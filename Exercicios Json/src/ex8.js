95ce76931d0d36663721c760d9eb56ea60c4f697
Felipe-ACG <adrianocezargomes@gmail.com>
2024-10-09 22:29:32 -0300

    Add files via upload

---
+++ Exercicios Json/src/ex8.js
@@ -0,0 +1,9 @@
+const disciplina = {
+    nome: "Algoritmos",
+    carga: 80,
+    pesos: [0.25,0.35,0.4],
+    notas: [8.2,7.5,9]
+};
+let {pesos} = disciplina
+console.log("Peso P1:", pesos[0])
+console.log("Peso P2:", pesos[1])
